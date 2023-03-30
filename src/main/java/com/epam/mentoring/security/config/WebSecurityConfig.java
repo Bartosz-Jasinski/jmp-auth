65e4a4e828d3bf0ecea0318d85d2b1e79a35204b
Bartosz-Jasinski <Bartosz_Jasinski@epam.com>
2023-03-30 16:37:38 +0200

    Add PostgreSQL

--- src/main/java/com/epam/mentoring/security/config/WebSecurityConfig.java
+++ src/main/java/com/epam/mentoring/security/config/WebSecurityConfig.java
@@ -1,17 +1,25 @@
 package com.epam.mentoring.security.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+	@Bean
+	public static PasswordEncoder getPasswordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
@@ -23,12 +31,7 @@ public class WebSecurityConfig {
 	}
 
 	@Bean
-	public InMemoryUserDetailsManager userDetailsService() {
-		UserDetails user = User.withDefaultPasswordEncoder()
-				.username("user@epam.com")
-				.password("password")
-				.roles("USER")
-				.build();
-		return new InMemoryUserDetailsManager(user);
+	public UserDetailsService users(DataSource dataSource) {
+		return new JdbcUserDetailsManager(dataSource);
 	}
 }
